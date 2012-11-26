fc33180d966cfde0962780c67e81c022e2045c34
Hadi Farnoud <hadi.farnoud@gmail.com>
2012-11-26 12:21:46 +0000

    initial commit

---
+++ growl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+__version__ = "0.1"
+__author__ = "Jim Pingle (http://pingle.org)"
+__copyright__ = "(C) 2011 Jim Pingle, original code (C) 2006 Rui Carmo. Code under BSD License."
+
+import sys, os
+from subprocess import call
+from regrowl import GrowlPacket
+from netgrowl import *
+from SocketServer import *
+
+password="openelec"
+
+class GrowlListener(UDPServer):
+# Hostname/IP to listen on. No automatic detection.
+	hostname = '0.0.0.0'
+	allow_reuse_address = True
+
+	def __init__(self, inpassword = None, outpassword = None):
+		self.inpassword = inpassword
+		self.outpassword = outpassword
+		UDPServer.__init__(self,(self.hostname, GROWL_UDP_PORT), _RequestHandler)
+	# end def
+
+	def server_close(self):
+		self.resolver.shutdown()
+	# end def
+# end class
+
+class _RequestHandler(DatagramRequestHandler):
+	dialogtimeout = '5'
+	kdialogpath = '/usr/bin/xbmc-send'
+	icon='http://growl.info/images/growlicons/HardwareGrowler_128.png'
+
+	def handle(self):
+		p = GrowlPacket(self.rfile.read(), self.server.inpassword,self.server.outpassword)
+		if p.type() == 'NOTIFY':
+			notification,title,description,app = p.info()
+			gtext = app + '\n' + notification + ': ' + description
+			call([self.kdialogpath, '-a "Notification(', title, ',', gtext, ',', self.dialogtimeout, ',', self.icon])
+
+if __name__ == "__main__":
+	r = GrowlListener(password,password)
+try:
+	r.serve_forever()
+except KeyboardInterrupt:
+	r.server_close()
